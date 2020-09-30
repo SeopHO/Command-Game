db54f76bfd94a78bb703de95f264b817bea91b72
SeopHO <56343122+SeopHO@users.noreply.github.com>
2020-10-01 02:43:49 +0900

    Add files via upload

--- script/Core.js
+++ script/Core.js
@@ -1,74 +1,31 @@
-let command_up={
+const command_up={
     text:'UP',
     code:38,
 }
-let command_down={
+const command_down={
     text:'DOWN',
     code:40,
 }
-let command_right={
+const command_right={
     text:'RIGHT',
     code:39,
 }
-let command_left={
+const command_left={
     text:'LEFT',
     code:37,
 }
-let command_wrong={
+const command_wrong={
     text:'X',
     code:0,
 }
 
-document.addEventListener("keydown", keyDownHandler, false);
-
-function keyDownHandler(event) 
-{
-    switch (event.keyCode) 
-    {
-        case command_up.code:
-            answer.append(command_up.text);
-            Commands_user.push(command_up.code);
-            console.log(Commands_user);
-            break;
-        case command_down.code:
-            answer.append(command_down.text);
-            Commands_user.push(command_down.code);
-            console.log(Commands_user);
-            break;
-        case command_right.code:
-            answer.append(command_right.text);
-            Commands_user.push(command_right.code);
-            console.log(Commands_user);
-            break;
-        case command_left.code:
-            answer.append(command_left.text);
-            Commands_user.push(command_left.code);
-            console.log(Commands_user);
-            break;
-        default:
-            answer.append('X ');
-            Commands_user.push(command_wrong.code);
-            console.log(Commands_user);
-    }
-};
-
-let question = document.querySelector('.question');
-let answer = document.querySelector('.answer');
-
-
-
-let Commands=[command_up, command_down, command_right, command_left]; //data
-let Commands_text=[command_up.text, command_down.text, command_right.text, command_left.text]; //print
+let Commands=[command_up, command_down, command_right, command_left];
+let Commands_text=[command_up.text, command_down.text, command_right.text, command_left.text];
 let Commands_user=[];
-
-let user_len;
-
 let Commands_len = Commands.length;
 
-function drawQuestion()
-{
-    question.append(Commands_text);
-}
+let Correct_count=0;
+let Wrong_count=0;
 
 function ShuffleCommands()
 {
@@ -94,14 +51,10 @@ function AddCommands()
         Commands_text.push(Commands_text[i]);
     }
     Commands_len+=Commands_len;
-    update();
 }
-
 
 function compareCommands()
 {
-    let Correct_count=-1;
-    let Wrong_count=-1;
     for(let i=0;i<Commands_len;i++)
     {
         if(Commands[i].code == Commands_user[i])
@@ -113,19 +66,16 @@ function compareCommands()
             Wrong_count++;
         }
     }
-    if(Correct_count === Commands_len)
-        alert("GOOD!");
-    else if(Wrong_count>-1)
-        alert("BAD");
+    console.log(Correct_count,Wrong_count);
 }
 
-
-function draw()
+function coreInit()
 {
     ShuffleCommands();
-    drawQuestion();
-    compareCommands();
+    questionInit();
 }
+
+coreInit();
 
 
 
---
+++ script/Question.js
@@ -0,0 +1,11 @@
+let question = document.querySelector('.question');
+
+function drawQuestion()
+{
+    question.append(Commands_text);
+}
+
+function questionInit()
+{
+    drawQuestion();
+}
