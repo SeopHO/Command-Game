49a86274d712a40524311a2c1b6803c69161a0a7
SeopHO <56343122+SeopHO@users.noreply.github.com>
2020-11-01 18:30:57 +0900

    Add files via upload

--- script/Graphic.js
+++ script/Graphic.js
@@ -1,8 +1,17 @@
 //https://medium.com/dailyjs/how-to-build-a-simple-sprite-animation-in-javascript-b764644244aa
 
 let queImage = new Image();
+let answImage = new Image();
 
-queImage.src = "";
+queImage.src = "./image/Answer.png";
+answImage.src = "./image/Question.png";
+
+
+
+document.body.appendChild(queImage);
+document.body.appendChild(answImage);
+
+
 
 
 
