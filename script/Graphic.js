d3efbef69cd7998603d943a23b51d3e0a8f9fe6c
SeopHO <56343122+SeopHO@users.noreply.github.com>
2020-11-10 10:46:01 +0900

    Add files via upload

--- script/Graphic.js
+++ script/Graphic.js
@@ -1,16 +1,26 @@
 //https://medium.com/dailyjs/how-to-build-a-simple-sprite-animation-in-javascript-b764644244aa
 
+let canvas = document.querySelector("canvas");
+let ctx = canvas.getContext("2d");
+
+let ca_width=1000;
+let ca_height=800;
+
+let qx = 200;
 let queImage = new Image();
 let answImage = new Image();
+
 
 queImage.src = "./image/Answer.png";
 answImage.src = "./image/Question.png";
 
+queImage.onload = ()=>{
+    ctx.drawImage(queImage,qx,(ca_height/2)-100);
+    ctx.drawImage(answImage,qx+500,ca_height/2-100);
+    ctx,drawImage()
+}
 
-
-document.body.appendChild(queImage);
-document.body.appendChild(answImage);
-
+console.log(screen.width, screen.height);
 
 
 
